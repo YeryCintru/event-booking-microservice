6152aab93b6e0e1ce552229b55136ed75d36bea6
nayerh24 <137061168+YeryCintru@users.noreply.github.com>
2026-09-18 13:45:47 +0200

    feat: JwtService y SecurityConfig

---
+++ services/auth-service/src/main/java/com/eventbooking/auth_service/security/JwtService.java
@@ -0,0 +1,27 @@
+package com.eventbooking.auth_service.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    
+    // Clave secreta para firmar y verificar los tokens JWT
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 86400000; // 1 día en milisegundos
+
+    public String generateToken(String email, String role){
+        return Jwts.builder()
+            .setSubject(email)
+            .claim("role", role)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(SECRET_KEY)
+            .compact();
+    }
+}
