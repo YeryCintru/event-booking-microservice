77f7443440ee01e12dbb808f0a202e06dee315e1
nayerh24 <137061168+YeryCintru@users.noreply.github.com>
2026-09-18 15:11:12 +0200

    feat: real token geneartion AuthServ

--- services/auth-service/src/main/java/com/eventbooking/auth_service/service/AuthService.java
+++ services/auth-service/src/main/java/com/eventbooking/auth_service/service/AuthService.java
@@ -3,16 +3,17 @@ package com.eventbooking.auth_service.service;
 import com.eventbooking.auth_service.dto.*;
 import com.eventbooking.auth_service.model.User;
 import com.eventbooking.auth_service.repository.UserRepository;
+import com.eventbooking.auth_service.security.JwtService;
 import lombok.RequiredArgsConstructor;
-
-import javax.management.RuntimeErrorException;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor 
 public class AuthService {
     private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthResponse register(RegisterRequest request){
         if (userRepository.existsByEmail(request.getEmail())){
@@ -21,15 +22,14 @@ public class AuthService {
 
         User user = User.builder() 
                 .email(request.getEmail())
-                .password(request.getPassword()) // Más adelante lo encriptaremos con BCrypt
+                .password(passwordEncoder.encode(request.getPassword())) // Password encriptada con BCrypt
                 .role("USER")
                 .build();
 
         userRepository.save(user);
 
         // Simulamos la devolución de token
-        String token = "mock-token-for" + user.getEmail(); // Aquí deberías generar un token JWT real
-
+        String token = jwtService.generateToken(user.getEmail(), user.getRole());
         return new AuthResponse(token);
     }
 
@@ -37,12 +37,12 @@ public class AuthService {
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
 
-        if (!user.getPassword().equals(request.getPassword())) {
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException("Contraseña incorrecta");                
         }
 
         // Simulamos la devolución de token
-        String token = "mock-token-for" + user.getEmail(); // Aquí deberías generar un token JWT real
+        String token = jwtService.generateToken(user.getEmail(), user.getRole()); 
 
         return new AuthResponse(token);
     }
